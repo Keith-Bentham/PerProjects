30963439808a6d00d249fa0adb2583fea8d7406c
keithbentham <keithbentham92@gmail.com>
2022-05-05 19:48:20 +0100

    .
    Signed-off-by: keithbentham <keithbentham92@gmail.com>

---
+++ ControlFlow.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ControlFlow {
+    public static void main(String[] ars) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.hasNext("test"));
+    }
+}
